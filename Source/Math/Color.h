b6f0df282dde3f40bb9cb3635dc47247c327351a
keichira <thmndl41@gmail.com>
2026-04-09 00:39:52 +0300

    fix msvc release build

--- Source/Math/Color.h
+++ Source/Math/Color.h
@@ -7,7 +7,7 @@ public:
     Color() : r(255), g(255), b(255), a(255) {}
     Color(uint8 r_, uint8 g_, uint8 b_) : r(r_), g(g_), b(b_), a(255) {}
     Color(uint8 r_, uint8 g_, uint8 b_, uint8 a_) : r(r_), g(g_), b(b_), a(a_) {}
-    Color(uint32 color) : r(color >> 24), g(color >> 16), b(b >> 8), a(color) {}
+    Color(uint32 color) : r(color >> 24), g(color >> 16), b(color >> 8), a(color) {}
 
 public:
     uint32 GetAsUINTSwap()
