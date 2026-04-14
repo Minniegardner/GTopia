71461967743dc59a6ec48cb21cfb2697714a7e4d
LyCellionX <foragtps@gmail.com>
2026-04-14 16:59:59 +0700

    Chemsynth lagiiii

--- GameServer/Event/UDP/GameMessage/SetSkin.h
+++ GameServer/Event/UDP/GameMessage/SetSkin.h
@@ -5,11 +5,4 @@
 class SetSkin {
 public:
     static void Execute(GamePlayer* pPlayer, ParsedTextPacket<8>& packet);
-};#pragma once
-
-#include "../../../Player/GamePlayer.h"
-
-class SetSkin {
-public:
-    static void Execute(GamePlayer* pPlayer, ParsedTextPacket<8>& packet);
 };
