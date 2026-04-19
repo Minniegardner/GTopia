c6d2945c610e2101485e664d09816057e8680d87
LyCellionX <foragtps@gmail.com>
2026-04-19 01:06:59 -0700

    Trade fix

---
+++ Build/compile.sh
@@ -0,0 +1,41 @@
+set -e
+
+echo "Select project to build:"
+echo "1) Master"
+echo "2) GameServer"
+echo "3) ItemManager"
+echo "4) WorldRenderer"
+read -n1 -p "Project: " project_choice
+echo ""
+
+case "$project_choice" in
+    1) PROJECT="Master" ;;
+    2) PROJECT="GameServer" ;;
+    3) PROJECT="ItemManager" ;;
+    4) PROJECT="WorldRenderer" ;;
+    *) echo "Invalid project type"
+       exit 1 ;;
+esac
+
+echo ""
+echo "Select mode to build:"
+echo "1) Debug"
+echo "2) Release"
+read -n1 -p "Build Mode: " mode_choice
+echo ""
+
+case "$mode_choice" in
+    1) MODE="Debug" ;;
+    2) MODE="Release" ;;
+    *) echo "Invalid build mode" 
+       exit 1 ;;
+esac    
+
+BUILD_DIR="build_$PROJECT"
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+
+echo "Building $PROJECT ($MODE)"
+cmake ../.. -DCMAKE_BUILD_TYPE=$MODE -DPROJECT_TO_BUILD=$PROJECT
+cmake --build .
+echo "$PROJECT build completed!"
