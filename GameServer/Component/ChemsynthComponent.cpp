3bb262f243779d35898f245fecc550daf8393dfe
LyCellionX <foragtps@gmail.com>
2026-04-20 15:06:22 -0700

    Trade fix

--- GameServer/Component/ChemsynthComponent.cpp
+++ GameServer/Component/ChemsynthComponent.cpp
@@ -1,33 +1,25 @@
 #include "ChemsynthComponent.h"
 
 #include "Algorithm/ChemsynthAlgorithm.h"
-#include "Utils/Timer.h"
-#include "World/TileInfo.h"
 #include "../World/World.h"
 #include "../World/WorldManager.h"
 
 namespace {
-constexpr uint64 kChemsynthTickIntervalMS = 1500;
+constexpr uint64 kChemsynthTickIntervalMS = 1500;  // Interval managed by algorithm internally
 
 bool s_enabled = true;
-Timer s_lastTick;
 }
 
 void ChemsynthComponent::SetEnabled(bool enabled)
 {
     s_enabled = enabled;
-    if(enabled) {
-        s_lastTick.Reset();
-    }
 }
 
 void ChemsynthComponent::OnTick()
 {
-    if(!s_enabled || s_lastTick.GetElapsedTime() < kChemsynthTickIntervalMS) {
+    if(!s_enabled) {
         return;
     }
-
-    s_lastTick.Reset();
 
     WorldManager* pWorldManager = GetWorldManager();
     if(!pWorldManager) {
@@ -39,21 +31,6 @@ void ChemsynthComponent::OnTick()
             return;
         }
 
-        WorldTileManager* pTileManager = pWorld->GetTileManager();
-        if(!pTileManager) {
-            return;
-        }
-
-        const Vector2Int size = pTileManager->GetSize();
-        for(int32 y = 0; y < size.y; ++y) {
-            for(int32 x = 0; x < size.x; ++x) {
-                TileInfo* pTile = pTileManager->GetTile(x, y);
-                if(!pTile || pTile->GetDisplayedItem() != ITEM_ID_CHEMSYNTH_PROCESSOR || !pTile->HasFlag(TILE_FLAG_IS_ON)) {
-                    continue;
-                }
-
-                ChemsynthAlgorithm::MoveActiveTank(pWorld, pTile);
-            }
-        }
+        ChemsynthAlgorithm::UpdateWorldChemsynth(pWorld);
     });
 }
--- GameServer/Component/GhostComponent.cpp
+++ GameServer/Component/GhostComponent.cpp
@@ -1,32 +1,25 @@
 #include "GhostComponent.h"
 
 #include "Algorithm/GhostAlgorithm.h"
-#include "Utils/Timer.h"
 #include "../World/World.h"
 #include "../World/WorldManager.h"
 
 namespace {
-constexpr uint64 kGhostTickIntervalMS = 300;
+constexpr uint64 kGhostTickIntervalMS = 300;  // Interval managed by algorithm internally
 
 bool s_enabled = true;
-Timer s_lastTick;
 }
 
 void GhostComponent::SetEnabled(bool enabled)
 {
     s_enabled = enabled;
-    if(enabled) {
-        s_lastTick.Reset();
-    }
 }
 
 void GhostComponent::OnTick()
 {
-    if(!s_enabled || s_lastTick.GetElapsedTime() < kGhostTickIntervalMS) {
+    if(!s_enabled) {
         return;
     }
-
-    s_lastTick.Reset();
 
     WorldManager* pWorldManager = GetWorldManager();
     if(!pWorldManager) {
